eb35e51b6f919dfcd9b060f96baefebf675948b9
heckfy811 <murtazin0811@gmail.com>
2026-05-26 01:10:12 +0300

    убрано лишнее

--- src/handlers/dp.py
+++ src/handlers/dp.py
@@ -1,13 +1,8 @@
 from maxapi import Dispatcher
 
 from . import admin_actions, consent, request_form, start, user_actions
-from ..database.db import AsyncSessionFactory
-from ..middleware.audit import AuditMiddleware
 
 dp = Dispatcher()
-
-# Регистрация middleware для записи событий в аудит-лог
-dp.register_outer_middleware(AuditMiddleware(AsyncSessionFactory))
 
 dp.include_routers(
     admin_actions.router,
